6032eb58583e97f162947698d075f0bcfbc5330e
Johnathan <johnathan@Admins-MacBook-Pro.local>
2017-03-01 07:22:04 -0800

    created data directory and a test.csv for testing, updated scoreUPDRSTest to test scoreUPDRS using test.csv

--- test/scoreUPDRSTest.R
+++ test/scoreUPDRSTest.R
@@ -6,50 +6,8 @@
 # source private scoreUPDRS.R functions
 source("UdallR/R/scoreUPDRS.R")
 
-# test data frame from data of original data frame 
-testdf <- data.frame(on_updrs_3a = c(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1), 
-                     on_updrs_3b = c(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, NA, 7, 7, NA, NA, 7, NA, NA, 1), 
-                     on_updrs_3c = c(1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 7, 7, 0, 0, 7, 0, 0, 1), 
-                     on_updrs_3_1 = c(1, 0, 0, 2, 2, 1, 2, 0, 1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1), 
-                     on_updrs_3_2 = c(1, 1, 0, 2, 2, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1), 
-                     on_updrs_3_3_neck = c(1, 2, 1, 2, 2, 1, 2, 1, 3, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1), 
-                     on_updrs_3_3_rue = c(1, 1, 1, 2, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 2), 
-                     on_updrs_3_3_lue = c(0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1), 
-                     on_updrs_3_3_rle = c(1, 1, 0, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1), 
-                     on_updrs_3_3_lle = c(2, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0), 
-                     on_updrs_3_4_r = c(1, 2, 0, 2, 1, 3, 3, 2, 1, 2, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1), 
-                     on_updrs_3_4_l = c(1, 3, 1, 2, 1, 3, 3, 1, 1, 2, 3, 0, 0, 0, 0, 0, 0, 1, 0, 0), 
-                     on_updrs_3_5_r = c(1, 1, 0, 2, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1), 
-                     on_updrs_3_5_l = c(1, 2, 1, 2, 0, 1, 2, 0, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0), 
-                     on_updrs_3_6_r = c(0, 1, 0, 1, 1, 1, 2, 1, 1, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0), 
-                     on_updrs_3_6_l = c(0, 2, 1, 2, 1, 2, 2, 1, 3, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0), 
-                     on_updrs_3_7_r = c(0, 2, 1, 2, 1, 2, 2, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0), 
-                     on_updrs_3_7_l = c(1, 2, 1, 3, 1, 1, 2, 2, 1, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0), 
-                     on_updrs_3_8_r = c(0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0), 
-                     on_updrs_3_8_l = c(1, 1, 1, 2, 1, 1, 1, 0, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0), 
-                     on_updrs_3_9 = c(0, 0, 0, 1, 3, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 2), 
-                     on_updrs_3_10 = c(1, 1, 0, 2, 1, 2, 1, 1, 1, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1), 
-                     on_updrs_3_11 = c(0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0), 
-                     on_updrs_3_12 = c(1, 1, 0, 3, 2, 3, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0), 
-                     on_updrs_3_13 = c(2, 1, 0, 2, 3, 2, 2, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2), 
-                     on_updrs_3_14 = c(0, 1, 1, 2, 2, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1), 
-                     on_updrs_3_15_r = c(0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-                     on_updrs_3_15_l = c(0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0), 
-                     on_updrs_3_16_r = c(0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1), 
-                     on_updrs_3_16_l = c(0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1), 
-                     on_updrs_3_17_rue = c(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0), 
-                     on_updrs_3_17_lue = c(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0), 
-                     on_updrs_3_17_rle = c(0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0), 
-                     on_updrs_3_17_lle = c(0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0), 
-                     on_updrs_3_17_lipjaw = c(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-                     on_updrs_3_18 = c(0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0), 
-                     on_updrs_3_hoehn_yahr = c(2, 2, 2, 3, 2, 3, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 2), 
-                     on_updrs_4_1 = c(5, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, NA, 10, 10, 10, 10, 10, NA, 10, 0), 
-                     on_updrs_4_2 = c(5, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, NA, 10, 10, 10, 10, 10, NA, 10, 4), 
-                     on_updrs_4_3 = c(5, 1, 1, 0, 1, 0, 0, 1, 0, 0, 2, NA, 10, 10, 10, 10, 10, NA, 10, 0), 
-                     on_updrs_4_4 = c(5, 1, 1, 0, 0, 0, 0, 1, 0, 0, 2, NA, 10, 10, 10, 10, 10, NA, 10, NA), 
-                     on_updrs_4_5 = c(5, 4, 3, 0, 1, 0, 0, 1, 0, 0, 2, NA, 10, 10, 10, 10, 10, NA, 10, 0), 
-                     on_updrs_4_6 = c(5, 0, 1, 1, 0, 0, 0, 2, 0, 0, 1, NA, 10, 10, 10, 10, 10, NA, 10, 0))
+# test data frame from data of original data frame, read as csv and as data.frame
+testdf <- as.data.frame(read.csv("UdallR/data/test.csv"))
 
 # Compares UPDRS score when desired (..., row, col, ...) dimensions are NA with expected vector.
 # 'row' is false when wanting to compare total with entire column as NA. 
@@ -60,10 +18,10 @@ compare_total_with_NA <- function(df, row, col, exp, total_col) {
     df[row, col] <- NA
   }
   obs <- scoreUPDRS(df)[, total_col] # total score if specified values are NA
-  stopifnot(identical(obs, exp)) # compare observed total score with expected
+  stopifnot(obs == exp) # compare observed total score with expected
 }
 
-# Functions testing scoreUPDRS(): An error will be produced if tests do not pass. 
+####### Functions testing scoreUPDRS(): An error will be produced if tests do not pass. 
 
 ## Test function for "on" UPDRS Part III
 scoreUPDRS3Test <- function(dat) {
@@ -71,53 +29,42 @@ scoreUPDRS3Test <- function(dat) {
   #### Test Missing ####
   ######################
   ### UPDRS PART III ###
-  # test row numbers 
-  middle_row = 10
-  last_row = 20
+  # row: 1 column: 153
+  compare_total_with_NA(dat, 1, 154, c(18, 25, 30, 11, 45, 29, 41, 33, 19, 37, 14, 25, 30, 17, 3, 1, 0, 0, 0, 2, 0, 1, 0, 19), "on_updrs_3_total")
   
-  # test column numbers
-  middle_col = 22
-  last_col = 37
+  # row: 20 column: 153
+  compare_total_with_NA(dat, 20, 154, c(19, 25, 30, 11, 45, 29, 41, 33, 19, 37, 14, 25, 30, 17, 3, 1, 0, 0, 0, 2, 0, 1, 0, 19), "on_updrs_3_total")
+ 
+  # row: 10 column: 153
+  compare_total_with_NA(dat, 10, 154, c(19, 25, 30, 11, 45, 29, 41, 33, 19, 35, 14, 25, 30, 17, 3, 1, 0, 0, 0, 2, 0, 1, 0, 19), "on_updrs_3_total")
   
-  # no extra NA values
-  stopifnot(identical(scoreUPDRS(dat)$on_updrs_3_total, c(19, 30, 11, 45, 41, 33, 37, 14, 25, 30, 17, 3, 1, 0, 0, 2, 0, 1, 0, 19)))
+  # row: 0 column: 153
+  compare_total_with_NA(dat, FALSE, 154, c(18, 24, 30, 11, 43, 28, 39, 32, 19, 35, 14, 24, 28, 17, 3, 1, 0, 0, 0, 2, 0, 1, 0, 18), "on_updrs_3_total")
   
-  # 1st column, 1st row
-  compare_total_with_NA(dat, 1, 1, c(19, 30, 11, 45, 41, 33, 37, 14, 25, 30, 17, 3, 1, 0, 0, 2, 0, 1, 0, 19), "on_updrs_3_total")
+  # row: 22 column: 172
+  compare_total_with_NA(dat, 22, 172, c(19, 25, 30, 11, 45, 29, 41, 33, 19, 37, 14, 25, 30, 17, 3, 1, 0, 0, 0, 2, 0, 1, 0, 19), "on_updrs_3_total")
+  
+  # row: 20 column: 172
+  compare_total_with_NA(dat, 20, 172, c(19, 25, 30, 11, 45, 29, 41, 33, 19, 37, 14, 25, 30, 17, 3, 1, 0, 0, 0, 2, 0, 1, 0, 19), "on_updrs_3_total")
+  
+  # row: 10 column: 172
+  compare_total_with_NA(dat, 10, 172, c(19, 25, 30, 11, 45, 29, 41, 33, 19, 36, 14, 25, 30, 17, 3, 1, 0, 0, 0, 2, 0, 1, 0, 19), "on_updrs_3_total")
+  
+  # row: 0 column: 172
+  compare_total_with_NA(dat, FALSE, 172, c(18, 24, 29, 11, 43, 28, 40, 31, 18, 36, 13, 24, 28, 16, 3, 1, 0, 0, 0, 2, 0, 1, 0, 18), "on_updrs_3_total")
+  
+  # row: 1 column: 196
+  compare_total_with_NA(dat, 1, 196, c(19, 25, 30, 11, 45, 29, 41, 33, 19, 37, 14, 25, 30, 17, 3, 1, 0, 0, 0, 2, 0, 1, 0, 19), "on_updrs_3_total")
+  
+  # row: 20 column: 196
+  compare_total_with_NA(dat, 20, 196, c(19, 25, 30, 11, 45, 29, 41, 33, 19, 37, 14, 25, 30, 17, 3, 1, 0, 0, 0, 2, 0, 1, 0, 19), "on_updrs_3_total")
+  
+  # row: 10 column: 196
+  compare_total_with_NA(dat, 10, 196, c(19, 25, 30, 11, 45, 29, 41, 33, 19, 37, 14, 25, 30, 17, 3, 1, 0, 0, 0, 2, 0, 1, 0, 19), "on_updrs_3_total")
+  
+  # row: 0 column: 196
+  compare_total_with_NA(dat, FALSE, 196, c(19, 25, 30, 11, 45, 29, 41, 33, 19, 37, 14, 25, 30, 17, 3, 1, 0, 0, 0, 2, 0, 1, 0, 19), "on_updrs_3_total")
 
-  # 1st column, last row 
-  compare_total_with_NA(dat, last_row, 1, c(19, 30, 11, 45, 41, 33, 37, 14, 25, 30, 17, 3, 1, 0, 0, 2, 0, 1, 0, 19), "on_updrs_3_total")
-
-  # 1st column, middle row
-  compare_total_with_NA(dat, middle_row, 1,  c(19, 30, 11, 45, 41, 33, 37, 14, 25, 30, 17, 3, 1, 0, 0, 2, 0, 1, 0, 19), "on_updrs_3_total")
-  
-  # entire 1st column
-  compare_total_with_NA(dat, FALSE, 1, c(19, 30, 11, 45, 41, 33, 37, 14, 25, 30, 17, 3, 1, 0, 0, 2, 0, 1, 0, 19), "on_updrs_3_total")
-  
-  # middle column, 1st row
-  compare_total_with_NA(dat, 1, middle_col, c(18, 30, 11, 45, 41, 33, 37, 14, 25, 30, 17, 3, 1, 0, 0, 2, 0, 1, 0, 19), "on_updrs_3_total")
-  
-  # middle column, last row 
-  compare_total_with_NA(dat, last_row, middle_col, c(19, 30, 11, 45, 41, 33, 37, 14, 25, 30, 17, 3, 1, 0, 0, 2, 0, 1, 0, 18), "on_updrs_3_total")
-  
-  # middle column, middle row
-  compare_total_with_NA(dat, middle_row, middle_col, c(19, 30, 11, 45, 41, 33, 37, 14, 25, 28, 17, 3, 1, 0, 0, 2, 0, 1, 0, 19), "on_updrs_3_total")
-  
-  # entire middle column
-  compare_total_with_NA(dat, FALSE, middle_col, c(18, 29, 11, 43, 40, 31, 36, 13, 24, 28, 16, 3, 1, 0, 0, 2, 0, 1, 0, 18), "on_updrs_3_total")
-  
-  # last column, 1st row
-  compare_total_with_NA(dat, 1, last_col, c(17, 30, 11, 45, 41, 33, 37, 14, 25, 30, 17, 3, 1, 0, 0, 2, 0, 1, 0, 19), "on_updrs_3_total")
-  
-  # 1ast column, last row
-  compare_total_with_NA(dat, last_row, last_col, c(19, 30, 11, 45, 41, 33, 37, 14, 25, 30, 17, 3, 1, 0, 0, 2, 0, 1, 0, 17), "on_updrs_3_total")
-  
-  # last column, middle row
-  compare_total_with_NA(dat, middle_row, last_col, c(19, 30, 11, 45, 41, 33, 37, 14, 25, 28, 17, 3, 1, 0, 0, 2, 0, 1, 0, 19), "on_updrs_3_total")
-
-  # entire last column
-  compare_total_with_NA(dat, FALSE, last_col, c(17, 28, 9, 42, 39, 30, 35, 12, 23, 28, 15, 3, 1, 0, 0, 2, 0, 1, 0, 17), "on_updrs_3_total")
-  
   # print if all tests passed 
   print("All tests were successful.")
 }
@@ -129,31 +76,21 @@ scoreUPDRS4Test <- function(dat) {
   ###########################
   #### TEST UPDRS PART IV ###
   
-  # test row numbers
-  middle_row = 10
-  last_row = 20
+  # row: 1, column: 192
+  compare_total_with_NA(dat, 1, 192, c(0, 5, 6, 8, 1, 5, 2, 0, 7, 0, 6, 0, 1, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4), "on_updrs_4_total")
   
-  # test colum numbers
-  last_col = 43
+  # row: 20, column: 195
+  compare_total_with_NA(dat, 20, 195, c(0, 5, 6, 8, 1, 5, 2, 0, 7, 0, 6, 0, 1, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4), "on_updrs_4_total")
   
-  # no NA
-  stopifnot(identical(scoreUPDRS(dat)$on_updrs_4_total, c(30, 6, 8, 1, 2, 0, 0, 6, 0, 1, 7, 0, 60, 60, 60, 60, 60, 0, 60, 4)))
+  # row: 10, column: 196
+  compare_total_with_NA(dat, 10, 196, c(0, 5, 6, 8, 1, 5, 2, 0, 7, 0, 6, 0, 1, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4), "on_updrs_4_total")
   
-  # first row, last column
-  compare_total_with_NA(dat, 1, last_col, c(25, 6, 8, 1, 2, 0, 0, 6, 0, 1, 7, 0, 60, 60, 60, 60, 60, 0, 60, 4), "on_updrs_4_total")
-  
-  # middle row, last column 
-  compare_total_with_NA(dat, middle_row, last_col, c(30, 6, 8, 1, 2, 0, 0, 6, 0, 1, 7, 0, 60, 60, 60, 60, 60, 0, 60, 4), "on_updrs_4_total")
-  
-  # last row, last column 
-  compare_total_with_NA(dat, last_row, last_col, c(30, 6, 8, 1, 2, 0, 0, 6, 0, 1, 7, 0, 60, 60, 60, 60, 60, 0, 60, 4), "on_updrs_4_total")
-  
-  # entire last row
-  compare_total_with_NA(dat, FALSE, last_col, c(25, 6, 7, 0, 2, 0, 0, 4, 0, 1, 6, 0, 50, 50, 50, 50, 50, 0, 50, 4), "on_updrs_4_total")
-  
+  # row: 0, column: 197
+  compare_total_with_NA(dat, 0, 197, c(0, 5, 6, 7, 0, 5, 2, 0, 4, 0, 4, 0, 1, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4), "on_updrs_4_total")
+
   # print if all tests passed 
   print("All tests were successful.")
-} 
+}
 
 # test on_updrs_3 items
 scoreUPDRS3Test(testdf)
