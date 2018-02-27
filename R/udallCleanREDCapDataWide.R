8397fef0a38932398ec793cb3959513988b03cc2
Trevor <tkmday@uw.edu>
2018-02-26 16:41:18 -0800

    update to newer multivis

--- R/udallCleanREDCapDataWide.R
+++ R/udallCleanREDCapDataWide.R
@@ -89,7 +89,7 @@ udallCleanREDCapDataWide <- function(dat, visit = 1) {
   # closest.colnames <- as.character(Codebook_PaNUC_2017_07_07$Stata_Variable_Name)
 
 
-  closest.colnames <- tolower(colnames(panuc_multivis_2018_01_25))
+  closest.colnames <- tolower(colnames(panuc_multivis_2018_01_26))
   closest.colnames <- closest.colnames[closest.colnames != ""]
   closest.colnames <- c("idnum",
                         closest.colnames[closest.colnames %in% colnames(dat)])
