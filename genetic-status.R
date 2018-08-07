eb74ed39954e43209c16d82f342b8d90a51098fb
Trevor <tkmday@uw.edu>
2018-08-07 16:36:38 -0700

    edit genetic program to work with partial information

--- genetic-status.R
+++ genetic-status.R
@@ -3,6 +3,7 @@
 
 # setwd("~")
 
+# Use Python-like optparse library (note: Python syntax for this)
 if (!require("optparse")) install.packages("optparse")
 library(optparse)
 
@@ -27,8 +28,8 @@ for (package in c("REDCapR", "devtools"))
   }
 }
 
+# Get date out of the opt object
 the.date <- opt$date
-
 message(paste("Using date", as.character(the.date)))
 
 # Install the custom UdallR library. You may have to change the path, depending
@@ -49,43 +50,76 @@ dat <- as.data.frame(redcap_read(redcap_uri = "https://redcap.iths.org/api/",
 
 # Test the UdallR cleaning function
 # The visit variable doesn't do much right now.
+# If opt$all is true, the user wants to keep all subjects. The clean function
+## wants to know if we are dropping the excluded file, so reverse opt$all.
 cdat <- udallCleanREDCapDataWide(dat, visit = 1, drop.excluded = !opt$all)
 
-cdat <- cdat[!is.na(cdat$idnum), c("idnum", "group", "on_mri_date", "apoe",
-                                   "gbastatus")]
+# The first four variables should be fairly self-explanatory.
+# apoe, gbastatus are the actual values, and redcap_* is a simple bool
+short <- cdat[!is.na(cdat$idnum), c("idnum", "group",
+                                   "off_mri_date", "on_mri_date",
+                                   "apoe", "redcap_apoe4",
+                                   "gbastatus", "redcap_gbacarrier")]
 
-# Were they scanned before the given date?
-scanned.before <- cdat$on_mri_date < as.Date(the.date)
-
-message(paste(sum(!scanned.before), "participants have been scanned since",
-                  the.date))
-
-cdat <- cdat[scanned.before, ]
-
-na.g <- sum(is.na(cdat$group))
+na.g <- sum(is.na(short$group))
 if (na.g > 0) {
-  message(paste(cdat$idnum[is.na(cdat$group)], collapse = ", "))
+  message(paste(short$idnum[is.na(short$group)], collapse = ", "))
   stop(paste(na.g, "subjects missing group status! Cannot proceed"))
 }
 
-pd  <- cdat[cdat$group == "pd", ]
-con <- cdat[cdat$group == "control", ]
+# Controls don't have off dates, make them NA
+short$off_mri_date[short$group == "control"] <- NA
 
-# Number of PD people who have an e4 allele and are GBA-
-pd.apoe4 <- grepl("4", pd$apoe) & pd$gbastatus == "Non-Carrier"
-pd.apoe4[is.na(pd$apoe)] <- NA
+# Is the subject an APOE carrier?
+# First, get the values from the apoe (the actual value) field
+short$is.apoe <- grepl("4", short$apoe)
+short$is.apoe[is.na(short$apoe)] <- NA
+# If it's still NA, then use the redcap_apoe4 field
+short$is.apoe[is.na(short$is.apoe)] <- as.logical(short$redcap_apoe4[is.na(short$is.apoe)])
+message(paste("After checking,", as.character(sum(is.na(short$is.apoe))),
+              "subjects are missing APOE records"))
 
-# Anyone who isn't GBA-
-pd.gbacarrier <- pd$gbastatus != "Non-Carrier"
+# Is the subject an GBA carrier?
+# First, get the values from the apoe (the actual value) field
+short$is.gba <- !grepl("Non", short$gbastatus)
+# If it's still NA, then use the redcap_apoe4 field
+short$is.gba[is.na(short$is.gba)] <- as.logical(short$redcap_gbacarrier[is.na(short$is.gba)])
+message(paste("After checking,", as.character(sum(is.na(short$is.gba))),
+              "subjects are missing GBA records"))
 
-# GBA+ and APOE e4-
-pd.noncarrier <- pd$gbastatus == "Non-Carrier" & !grepl("4", pd$apoe)
+# Okay, but now APOE e4+ subjects don't count as APOE if they are also GBA+
+both <- short$is.apoe & short$is.gba
+short$is.apoe[both] <- FALSE
 
-con.apoe4 <- grepl("4", con$apoe) & con$gbastatus == "Non-Carrier"
-con.apoe4[is.na(con$apoe)] <- NA
+# Use the first date they were scanned, ignoring any repeats, and drop NA to
+## use the single visit date for controls
+short$date <- apply(short[, c("on_mri_date", "off_mri_date")], 1, min,
+                    na.rm = TRUE)
 
-con.gbacarrier <- con$gbastatus != "Non-Carrier"
-con.noncarrier <- con$gbastatus == "Non-Carrier" & !grepl("4", con$apoe)
+na.dates <- sum(is.na(as.Date(short$date, format = "%Y-%m-%d")))
+if (na.dates > 0)
+  stop(paste("There are", na.dates, "subjects with missing dates.",
+             "Cannot proceed."))
+
+
+# Were they scanned before the given date?
+scanned.before <- short$date < as.Date(the.date)
+short <- short[scanned.before, ]
+
+message(paste(sum(!scanned.before), "participants have been scanned since",
+                  as.character(the.date)))
+
+# Split up into groups
+pd  <- short[short$group == "pd", ]
+con <- short[short$group == "control", ]
+
+pd.apoe4 <- sum(pd$is.apoe, na.rm = TRUE)
+pd.gbacarrier <- sum(pd$is.gba, na.rm = TRUE)
+pd.noncarrier <- sum(!pd$is.gba & !pd$is.apoe, na.rm = TRUE)
+
+con.apoe4 <- sum(con$is.apoe, na.rm = TRUE)
+con.gbacarrier <- sum(con$is.gba, na.rm = TRUE)
+con.noncarrier <- sum(!con$is.gba & !con$is.apoe, na.rm = TRUE)
 
 sum.NA <- function(v, category)
 {
@@ -100,7 +134,7 @@ sum.NA <- function(v, category)
 message("")
 message(paste0("N PD: ", nrow(pd)))
 message(paste0("N Control: ", nrow(con)))
-message(paste0("Total: ", nrow(cdat)))
+message(paste0("Total: ", nrow(short)))
 
 message("")
 sum.NA(pd.apoe4,      "PD APOE e4+    ")
@@ -109,19 +143,25 @@ sum.NA(pd.noncarrier, "PD non-carriers")
 
 message("")
 sum.NA(con.apoe4,      "Control APOE e4+")
-# sum.NA(con.gbacarrier, "PD GBA+")
+sum.NA(con.gbacarrier, "-- Control GBA+")
 sum.NA(con.noncarrier, "Control non-carriers")
 
 # WHICH IDS ARE MISSING INFO?
 message("")
 
-message(paste("No APOE status (PD):\t",
-              paste(pd$idnum[is.na(pd.apoe4)], collapse = " ")))
-message(paste("No GBA status (PD):\t",
-              paste(pd$idnum[is.na(pd.gbacarrier)], collapse = " ")))
+if (sum(is.na(pd.apoe4)) > 0)
+  message(paste("No APOE status (PD):\t",
+                paste(pd$idnum[is.na(pd.apoe4)], collapse = " ")))
 
-message(paste("No APOE status (C):\t",
-              paste(con$idnum[is.na(con.apoe4)], collapse = " ")))
+if (sum(is.na(pd.gbacarrier)) > 0)
+  message(paste("No GBA status (PD):\t",
+                paste(pd$idnum[is.na(pd.gbacarrier)], collapse = " ")))
+
+if (sum(is.na(con.apoe4)) > 0)
+  message(paste("No APOE status (C):\t",
+                paste(con$idnum[is.na(con.apoe4)], collapse = " ")))
+
+if (sum(is.na(con.gbacarrier)) > 0)
 message(paste("No GBA status (C):\t",
               paste(con$idnum[is.na(con.gbacarrier)], collapse = " ")))
 
