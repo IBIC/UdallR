a970b97290e645f21fe4df3fda260c88bde6517d
Trevor <tkmday@uw.edu>
2018-08-10 15:36:25 -0700

    update multivis and add script to merge Brian's new separated multivis files

---
+++ merge-multivis.R
@@ -0,0 +1,32 @@
+setwd("~/UdallR/data")
+
+args <- commandArgs(trailingOnly = TRUE)
+dir <- args[1]
+
+if (length(args) == 0) dir <- "180809"
+
+if (!dir.exists(dir))
+  stop("No such directory")
+
+# Read input files
+files <- list.files(dir, pattern = "panuc-0110-.*.csv", full.names = TRUE)
+updrs <- read.csv(files[1])
+main  <- read.csv(files[2])
+
+# These are the columns shared by both data frames, minus the ones were actually
+# going to merge on
+in.common <- intersect(colnames(updrs), colnames(main))[-(1:2)]
+
+# Drop those columns since the data already exists and merging on it just
+# creates more problems
+main.merge <- main[, !(colnames(main) %in% in.common)]
+
+# Actually merge the data
+merged <- merge(updrs, main.merge, by = c("summary_id", "visit_number"))
+
+# Output file name
+output.date <- gsub(".*/panuc-0110-", "",
+                    gsub(".csv", "", files[2]))
+output.file <- paste0("~/UdallR/data/panuc_multivis_", output.date, ".csv")
+
+write.csv(merged, file = output.file, row.names = FALSE)
